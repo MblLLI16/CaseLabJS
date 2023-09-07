b625dc4f98ffe1c6058a90813fdcc91ffa81a7b9
MblLLI16 <mbllli16@mail.ru>
2023-09-07 19:31:22 +0800

    task-list stile

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,6 +4,7 @@ const addButton = document.getElementById('add');
 
 addButton.addEventListener('click', () => {
     const userInput = document.getElementsByTagName('input')[0];
-    if (userInput.value === "") console.log('Ввод пуст')
-    else console.log(userInput.value);
+    if (userInput.value) {
+        
+    }
 })
