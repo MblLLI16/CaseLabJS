fe99cfb1dc280f739a574295ab4083b794c20076
MblLLI16 <mbllli16@mail.ru>
2023-09-08 12:37:56 +0800

    experiments with js

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,6 +5,24 @@ const addButton = document.getElementById('add');
 addButton.addEventListener('click', () => {
     const userInput = document.getElementsByTagName('input')[0];
     if (userInput.value) {
-        
+        addItemTodoList(userInput.value);
+        userInput.value = '';
     }
 })
+
+function addItemTodoList(text) {
+    const todoList = document.querySelector('.todo');
+
+    const listItem = document.createElement('li');
+    listItem.innerText = text;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', () => {
+        todoList.removeChild(listItem);
+    });
+
+    listItem.appendChild(deleteButton);
+    todoList.appendChild(listItem);
+}
