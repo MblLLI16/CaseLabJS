b38958211bd4f0341289b2e14906d4413617c785
MblLLI16 <mbllli16@mail.ru>
2023-09-12 01:25:48 +0800

    js upd

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,7 @@
 console.log('lol')
 
 const addButton = document.getElementById('add');
+const input = document.querySelector('.styled-input');
 
 addButton.addEventListener('click', () => {
     const userInput = document.getElementsByTagName('input')[0];
@@ -10,19 +11,61 @@ addButton.addEventListener('click', () => {
     }
 })
 
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const userInput = document.getElementsByTagName('input')[0];
+        if (userInput.value) {
+            addItemTodoList(userInput.value);
+            userInput.value = '';
+        }
+    }
+})
+
 function addItemTodoList(text) {
     const todoList = document.querySelector('.todo');
+    const completed = document.querySelector('.completed');
 
     const listItem = document.createElement('li');
     listItem.innerText = text;
 
-    const deleteButton = document.createElement('button');
-    deleteButton.style.backgroundImage = 'url("./resources/assets/Vector.svg")';
-    deleteButton.innerText = '';
-    deleteButton.addEventListener('click', () => {
-        todoList.removeChild(listItem);
+    const checkButton = document.createElement('button');
+    checkButton.className = 'check-button';
+    //svg
+    const checkIcon = document.createElement('img');
+    checkIcon.src = './resources/assets/unCheckIcon.svg';
+    checkIcon.alt = 'Check Icon';
+    checkButton.appendChild(checkIcon);
+
+    checkButton.addEventListener('click', () => {
+        if (todoList.contains(listItem)) {
+            completed.appendChild(listItem);
+            listItem.classList.add('.completed')
+            checkIcon.src = './resources/assets/CheckIcon.svg';
+        } else if (completed.contains(listItem)) {
+            todoList.appendChild(listItem);
+            listItem.classList.add('.todo')
+            checkIcon.src = './resources/assets/unCheckIcon.svg';
+        } else (console.log('Ошибка checkButton'))
+    })
+
+    const delButton = document.createElement('button');
+    delButton.className = 'del-button';
+    //svg
+    const delIcon = document.createElement('img');
+    delIcon.src = './resources/assets/Vector.svg';
+    delIcon.alt = 'Delete Icon'
+    delButton.appendChild(delIcon);
+
+    delButton.addEventListener('click', () => {
+        if (todoList.contains(listItem)) {
+            todoList.removeChild(listItem);
+        } else if (completed.contains(listItem)) {
+            completed.removeChild(listItem);
+        } else (console.log('Ошибка delButton'))
+
     });
 
-    listItem.appendChild(deleteButton);
+    listItem.appendChild(checkButton);
+    listItem.appendChild(delButton);
     todoList.appendChild(listItem);
 }
